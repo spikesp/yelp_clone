a2e31a6cd359b910fe60fc9d5832018e5adcf00d
spike <weisuojiaoshou@gmail.com>
2018-01-17 12:23:57 +0900

    finish add business

--- yelp/app/controllers/businesses_controller.rb
+++ yelp/app/controllers/businesses_controller.rb
@@ -2,4 +2,22 @@ class BusinessesController < ApplicationController
   def index
     @businesses = Business.all
   end
+
+  def new
+    @business = Business.new
+  end
+
+  def create
+    @business = Business.new(business_params)
+    if @business.save
+      redirect_to :home
+    else
+      render :home
+    end
+  end
+
+  private
+  def business_params
+    params.require(:business).permit(:title, :description, :url)
+  end
 end
--- yelp/config/routes.rb
+++ yelp/config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get 'ui(/:action)', controller: 'ui'
 
   get 'home', to: "businesses#index"
+  post 'home', to: "businesses#create"
 end
