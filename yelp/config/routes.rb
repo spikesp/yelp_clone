01a83b527e8729a991f01383b3bba18935d5e2ab
spike <weisuojiaoshou@gmail.com>
2018-01-17 16:44:40 +0900

    Finish the function of User Registrtion

--- yelp/config/routes.rb
+++ yelp/config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
+  root to: "businesses#index"
   get 'ui(/:action)', controller: 'ui'
 
   get 'home', to: "businesses#index"
   post 'home', to: "businesses#create"
+
+  
+  resources :sessions, only: [:create]
+
+  get 'sign_up', to: "users#new"
+  post 'sign_up', to: "users#create"
+  get 'sign_in', to: 'sessions#new'
+  resources :sessions, only: [:create]
+
+  get 'sign_out', to: 'sessions#destroy'
 end
--- yelp/db/schema.rb
+++ yelp/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180117020241) do
+ActiveRecord::Schema.define(version: 20180117052458) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 20180117020241) do
     t.string "title"
     t.string "description"
     t.string "url"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "email"
+    t.string   "password_digest"
+    t.string   "full_name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
